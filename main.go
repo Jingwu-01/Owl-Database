e20d2a5fc707cc1e7890b08c520be207cc59ea6d
wellsch <games@smcewells.com>
2023-09-08 21:06:23 -0500

    GET stuff ready to go

--- main.go
+++ main.go
@@ -1,46 +1,178 @@
-// This file is a skeleton for your project. You should replace this
-// comment with true documentation.
+/*
+OwlDB stores documents in a hirearchical database structure.
+The documents are JSON values encoded in UTF-8. The database
+imposes a schema against which all documents are verified.
+
+The DB stores a set of zero or more top-level databases, which
+each point to zero or more documents. The documents contain
+server-created metadata as well as user-supplied content. They
+also point to collections, which are not part of the output to
+the client, but contain further documents.
+
+Usage:
+
+	owldb [flags]
+
+The flags are:
+
+	-p
+		A port number, an integer greater than 1024. If omitted,
+		the server will listen to port 3318.
+	-s
+		A JSON schema file name, the file which contains the JSON
+		schema that will be used to validate all the documents stored
+		in the database. This flag must be included and the file must
+		be a valid JSON schema file.
+	-t
+		A token file name, the file contains a JSON mapping user names
+		to string tokens. These tokens will be installed on the system
+		for 24 hours.
+
+When a client connects to the OwlDB server, they will be given a unique
+token which they will use on all future logins. They will have the power
+to then access all of the databases, documents, and collections on the
+server, as well as adding new ones, and subscribing to changes.
+*/
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"os/signal"
+	"strings"
+	"sync"
 	"syscall"
 
 	"github.com/santhosh-tekuri/jsonschema/v5"
 )
 
+// A meta stores metadata about a document.
+type meta struct {
+	createdBy      string
+	createdAt      int
+	lastModifiedBy string
+	lastModifiedAt int
+}
+
+/*
+A collection is a concurrent skip list of documents,
+which is sorted by document name.
+*/
+type collection struct {
+	documents map[string]document
+	mu        sync.Mutex
+}
+
+/*
+A docoutput is a struct which represents the data to
+be output when a user requests a given document.
+*/
+type docoutput struct {
+	path     string
+	meta     meta
+	contents []byte
+}
+
+// A document is a document plus a concurrent skip list of collections
+type document struct {
+	output   docoutput
+	children map[string]collection
+	mu       sync.Mutex
+}
+
+// A dbhandler is the highest level struct, holds all the collections and
+// handles all the http requests.
+type dbhandler struct {
+	databases map[string]collection
+	mu        sync.Mutex
+}
+
+// Creates a new DBHandler
+func NewDBHandler() dbhandler {
+	return dbhandler{make(map[string]collection), sync.Mutex{}}
+}
+
+// The server implements the "handler" interface, it will recieve
+// requests from the user and delegate them to the proper methods.
+func (d *dbhandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		path, found := strings.CutPrefix(r.URL.Path, "/v1/")
+
+		// Check for version
+		if !found {
+			slog.Info("User path did not include version", "path", path)
+			msg := fmt.Sprintf("path missing version: %s", path)
+			http.Error(w, msg, http.StatusBadRequest)
+		}
+
+		splitpath := strings.SplitAfterN(path, "/", 2)
+		dbpath := splitpath[0]
+
+		d.mu.Lock()
+		database, ok := d.databases[dbpath]
+		d.mu.Unlock()
+
+		// Check to see if database exists
+		if !ok {
+			slog.Info("User attempted to access non-extant database", "path", path)
+			msg := fmt.Sprintf("Invalid database: %s", path)
+			http.Error(w, msg, http.StatusBadRequest)
+		}
+
+		if len(splitpath) == 1 {
+			// GET database case
+			fmt.Printf("database.documents: %v\n", database.documents)
+		} else {
+			// GET document/collection case
+			path = splitpath[1]
+			fmt.Printf(path)
+		}
+	case http.MethodPut:
+		// Put handling
+	//case http.MethodPost:
+	// Post handling
+	//case http.MethodPatch:
+	// Patch handling
+	//case http.MethodDelete:
+	// Delete handling
+	//case http.MethodOptions:
+	// Options handling
+	default:
+		// If user used method we do not support.
+		slog.Info("User used unsupported method", "method", r.Method)
+		msg := fmt.Sprintf("unsupported method: %s", r.Method)
+		http.Error(w, msg, http.StatusBadRequest)
+	}
+}
+
+// Implements the main functionality for the OwlDB program.
 func main() {
+	// Declare variables for main method.
 	var schema *jsonschema.Schema
+	var tknPath string
 	var server http.Server
+	var handler dbhandler
 	var port int
 	var err error
 
-	// Initialize flags
-	portFlag := flag.Int("p", 3318, "Port number")
-	schemaFlag := flag.String("s", "", "Schema file name")
-	tokenFlag := flag.String("t", "", "Token file name")
+	// Initialize the user input variables.
+	port, schema, tknPath, err = initialize()
 
-	flag.Parse()
-
-	port = *portFlag
-
-	if *schemaFlag == "" {
-		slog.Error("Missing schema", "error")
+	// Printing was handled in initialize.
+	if err != nil {
 		return
 	}
 
-	schema, err = jsonschema.Compile(*schemaFlag)
+	handler = NewDBHandler()
+	server.Addr = fmt.Sprintf("localhost:%d", port)
+	server.Handler = &handler
 
-	if err != nil {
-		slog.Error("Invalid schema", "error", err)
-	}
-
-	fmt.Println(tokenFlag)
+	fmt.Println(tknPath)
 	fmt.Println(schema.Location)
 
 	// The following code should go last and remain unchanged.
@@ -64,4 +196,46 @@ func main() {
 	} else {
 		slog.Info("Server closed", "error", err)
 	}
+}
+
+// Initialize sets up flags for inputs and compiles
+// the input schema file into a jsonschema.Schema object.
+// Returns the port number, schema object, and the token
+// file's path.
+func initialize() (int, *jsonschema.Schema, string, error) {
+	// Initialize flags
+	portFlag := flag.Int("p", 3318, "Port number")
+	schemaFlag := flag.String("s", "", "Schema file name")
+	tokenFlag := flag.String("t", "", "Token file name")
+	loggerFlag := flag.Int("l", 0, "Logger output level, -1 for debug, 1 for only errors")
+	flag.Parse()
+
+	// Ensure we got a schema file
+	if *schemaFlag == "" {
+		slog.Error("Missing schema", "error", errors.New("Missing schema"))
+		return 0, nil, "", errors.New("Missing Schema")
+	}
+
+	// Compile the schema
+	schema, err := jsonschema.Compile(*schemaFlag)
+
+	// Check for errors.
+	if err != nil {
+		slog.Error("Invalid schema", "error", err)
+		return 0, nil, "", errors.New("Invalid schema")
+	}
+
+	// Set to debug and above
+	if *loggerFlag == -1 {
+		h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelDebug})
+		slog.SetDefault(slog.New(h))
+	}
+
+	// Set to error only
+	if *loggerFlag == 1 {
+		h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{Level: slog.LevelError})
+		slog.SetDefault(slog.New(h))
+	}
+
+	return *portFlag, schema, *tokenFlag, nil
 }
