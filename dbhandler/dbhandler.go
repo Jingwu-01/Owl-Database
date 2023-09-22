9a4f12e5d73ec4d58e2f02e8dbdd5d0e5631fc7a
Jingwu Wang <jingwu@wangjingwudeMacBook-Pro.local>
2023-09-22 14:29:08 -0500

    login and logout enabled

--- dbhandler/dbhandler.go
+++ dbhandler/dbhandler.go
@@ -394,136 +394,178 @@ func (d *Dbhandler) Options(w http.ResponseWriter, r *http.Request) {
 
 // Handles case where we have DELETE request.
 func (d *Dbhandler) Delete(w http.ResponseWriter, r *http.Request) {
-	// Set headers of response
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// Check if is a logout request. if yes, logout; otherwise continue.
+	if strings.Contains(r.URL.Path, "auth") {
+		// Logout request case
 
-	path, found := strings.CutPrefix(r.URL.Path, "/v1/")
+		// check whether token is missing
+		token := r.Header.Get("Authorization")
+		if token == "" {
+			slog.Info("token is missing", "token", token)
+			http.Error(w, "Missing or invalid bearer token", http.StatusUnauthorized)
+			return
+		}
 
-	// Check for version
-	if !found {
-		slog.Info("User path did not include version", "path", path)
-		msg := fmt.Sprintf("path missing version: %s", path)
-		http.Error(w, msg, http.StatusBadRequest)
-		return
-	}
+		// Validate token and expiration in sessions map
+		userInfo, ok := d.sessions.Load(token)
+		if ok {
+			if userInfo.(SessionInfo).ExpiresAt.After(time.Now()) {
+				// Remove the corresponding userInfo from the sessions map
+				d.sessions.Delete(token)
+				w.WriteHeader(http.StatusNoContent)
+				slog.Info("user is successfully removed")
+				return
+			} else {
+				// token has expired
+				slog.Info("token has expired")
+				http.Error(w, "Missing or invalid bearer token", http.StatusUnauthorized)
+				return
+			}
 
-	splitpath := strings.SplitAfterN(path, "/", 2)
-
-	dbpath, err := decoder.PercentDecoding(splitpath[0])
-
-	// Error messages printed in percentDecoding function
-	if err != nil {
-		msg := fmt.Sprintf("Error translating hex encoding: %s", err.Error())
-		http.Error(w, msg, http.StatusInternalServerError)
-		return
-	}
-
-	// Check to see if database exists
-	database, ok := d.databases.Load(dbpath)
-	// If the database does not exist, return StatusNotFound error
-	if !ok {
-		slog.Info("User attempted to access non-extant database", "db", dbpath)
-		msg := fmt.Sprintf("Database does not exist")
-		http.Error(w, msg, http.StatusNotFound)
-		return
-	}
-
-	if len(splitpath) == 1 {
-		// DELETE database case
-		d.databases.Delete(dbpath)
-		slog.Info("Deleted Database", "path", dbpath)
-		w.Header().Set("Location", r.URL.Path)
-		w.WriteHeader(http.StatusNoContent)
-		return
+		} else {
+			// token does not exist
+			slog.Info("token does not exist")
+			http.Error(w, "Missing or invalid bearer token", http.StatusUnauthorized)
+			return
+		}
 
 	} else {
-		// DELETE document case
+		// Set headers of response
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		// Decode the document name
-		docpath, _ := strings.CutSuffix(splitpath[1], "/")
-		docpath, err1 := decoder.PercentDecoding(docpath)
-		if err1 != nil {
-			msg := fmt.Sprintf("Error translating hex encoding: %s", err.Error())
+		path, found := strings.CutPrefix(r.URL.Path, "/v1/")
+
+		// Check for version
+		if !found {
+			slog.Info("User path did not include version", "path", path)
+			msg := fmt.Sprintf("path missing version: %s", path)
 			http.Error(w, msg, http.StatusBadRequest)
 			return
 		}
 
-		// Access the document
-		_, exist := database.(collection).documents.Load(docpath)
-		if exist {
-			// Document found
-			database.(collection).documents.Delete(docpath)
-			slog.Info("Deleted Document", "path", path)
+		splitpath := strings.SplitAfterN(path, "/", 2)
+
+		dbpath, err := decoder.PercentDecoding(splitpath[0])
+
+		// Error messages printed in percentDecoding function
+		if err != nil {
+			msg := fmt.Sprintf("Error translating hex encoding: %s", err.Error())
+			http.Error(w, msg, http.StatusInternalServerError)
+			return
+		}
+
+		// Check to see if database exists
+		database, ok := d.databases.Load(dbpath)
+		// If the database does not exist, return StatusNotFound error
+		if !ok {
+			slog.Info("User attempted to access non-extant database", "db", dbpath)
+			msg := fmt.Sprintf("Database does not exist")
+			http.Error(w, msg, http.StatusNotFound)
+			return
+		}
+
+		if len(splitpath) == 1 {
+			// DELETE database case
+			d.databases.Delete(dbpath)
+			slog.Info("Deleted Database", "path", dbpath)
 			w.Header().Set("Location", r.URL.Path)
 			w.WriteHeader(http.StatusNoContent)
 			return
+
 		} else {
-			// Document not found
-			slog.Error("Document does not exist")
-			http.Error(w, "Document does not exist", http.StatusNotFound)
-			return
+			// DELETE document case
+
+			// Decode the document name
+			docpath, _ := strings.CutSuffix(splitpath[1], "/")
+			docpath, err1 := decoder.PercentDecoding(docpath)
+			if err1 != nil {
+				msg := fmt.Sprintf("Error translating hex encoding: %s", err.Error())
+				http.Error(w, msg, http.StatusBadRequest)
+				return
+			}
+
+			// Access the document
+			_, exist := database.(collection).documents.Load(docpath)
+			if exist {
+				// Document found
+				database.(collection).documents.Delete(docpath)
+				slog.Info("Deleted Document", "path", path)
+				w.Header().Set("Location", r.URL.Path)
+				w.WriteHeader(http.StatusNoContent)
+				return
+			} else {
+				// Document not found
+				slog.Error("Document does not exist")
+				http.Error(w, "Document does not exist", http.StatusNotFound)
+				return
+			}
 		}
 	}
 }
 
 // Handles case where we have POST request.
 func (d *Dbhandler) Post(w http.ResponseWriter, r *http.Request) {
-	// Assume it is a User Login request here, need to change it later
 
-	// Set headers of response
-	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	if strings.Contains(r.URL.Path, "auth") {
+		// Login request case
 
-	// Read body of requests
-	desc, err := io.ReadAll(r.Body)
-	defer r.Body.Close()
-	if err != nil {
-		slog.Error("Login: error reading the request body", "error", err)
-		http.Error(w, `"invalid login format"`, http.StatusBadRequest)
-		return
+		// Set headers of response
+		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		// Read body of requests
+		desc, err := io.ReadAll(r.Body)
+		defer r.Body.Close()
+		if err != nil {
+			slog.Error("Login: error reading the request body", "error", err)
+			http.Error(w, `"invalid login format"`, http.StatusBadRequest)
+			return
+		}
+
+		// Read Body data
+		var userInfo map[string]string
+		err = json.Unmarshal(desc, &userInfo)
+		if err != nil {
+			slog.Error("Login: error unmarshaling request", "error", err)
+			http.Error(w, `"invalid login format"`, http.StatusBadRequest)
+			return
+		}
+
+		// Validate against schema
+		err = d.schema.Validate(userInfo)
+		if err != nil {
+			slog.Error("Login: request body did not conform to schema", "error", err)
+			http.Error(w, `"Login: request body did not conform to schema"`, http.StatusBadRequest)
+			return
+		}
+
+		// Generate a secure, random token
+		token, err := generateToken()
+		if err != nil {
+			slog.Error("Login: token not successfully generated", "error", err)
+			http.Error(w, "Login: token not successfully generated", http.StatusInternalServerError)
+			return
+		}
+
+		// Store username and token in a session map with expiration time
+		username := userInfo["username"]
+		d.sessions.Store(token, SessionInfo{Username: username, ExpiresAt: time.Now().Add(1 * time.Hour)})
+
+		// Return the token to the user
+		jsonToken, err := json.Marshal(map[string]string{"token": token})
+		if err != nil {
+			// This should never happen
+			slog.Error("Login: error marshaling", "error", err)
+			http.Error(w, `"internal server error"`, http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(jsonToken)
+		slog.Info("Login: success")
+	} else {
+		// Handle other cases
 	}
-
-	// Read Body data
-	var userInfo map[string]string
-	err = json.Unmarshal(desc, &userInfo)
-	if err != nil {
-		slog.Error("Login: error unmarshaling request", "error", err)
-		http.Error(w, `"invalid login format"`, http.StatusBadRequest)
-		return
-	}
-
-	// Validate against schema
-	err = d.schema.Validate(userInfo)
-	if err != nil {
-		slog.Error("Login: request body did not conform to schema", "error", err)
-		http.Error(w, `"Login: request body did not conform to schema"`, http.StatusBadRequest)
-		return
-	}
-
-	// Generate a secure, random token
-	token, err := generateToken()
-	if err != nil {
-		slog.Error("Login: token not successfully generated", "error", err)
-		http.Error(w, "Login: token not successfully generated", http.StatusInternalServerError)
-		return
-	}
-
-	// Store username and token in a session map with expiration time
-	username := userInfo["username"]
-	d.sessions.Store(token, SessionInfo{Username: username, ExpiresAt: time.Now().Add(1 * time.Hour)})
-
-	// Return the token to the user
-	jsonToken, err := json.Marshal(map[string]string{"token": token})
-	if err != nil {
-		// This should never happen
-		slog.Error("Login: error marshaling", "error", err)
-		http.Error(w, `"internal server error"`, http.StatusInternalServerError)
-		return
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonToken)
-	slog.Info("Login: success")
 }
 
 // generateToken is a helper function that generates a secure, random token
